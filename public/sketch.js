50a837e231e27342a397facf3033f248d160e40a
chriscalver <130410868+chriscalver@users.noreply.github.com>
2024-10-18 16:35:06 -0400

    non ssl

--- public/sketch.js
+++ public/sketch.js
@@ -1,124 +1,206 @@
-// Keep track of our socket connection
 var socket;
 let ellipses = [];
 let IncomingMessage = "";
 let OutgoingMessage = "";
-let manifest = "Welcome to the P5.js Socket.IO Chat App";
+let manifest = "";
 let inputBox;
 let submitButton;
 let incomingBubble = false;
 let outgoingBubble = false;
 let headerBubble = true;
-let userInput; // Define userInput as a global variable
+let player1 = "Anonymous";
+let player2 = "Anonymous";
+let users = [];
+let userHeader = "Users Connected: ";
+let userNames = [];
+let position =  180;
 
-window.onload = function() {
-  showCustomPrompt();
-};
+let userName = prompt("Please enter your name:");
+if (userName) {
+  // player1 = userName;
+  localStorage.setItem("userName", userName);
+  manifest = `Welcome to the P5.js Socket.IO Chat App, ${userName}!`;
+}
 
 function setup() {
-  createCanvas(1200, 600);
+  createCanvas(1000, 500);
+  // createCanvas(500, 500);
   background(0);
   textSize(18);
   textFont("Arial");
+  socket = io.connect("https://chriscalver.com:8080");
+  // socket = io.connect("http://localhost:8080");
+  
+  
+  socket.on("connect", function () {
+    console.log("Connected");
+    socket.emit("addNewUser", userName);
+    // socket.emit("addNewUser", localStorage.getItem("userName"));
+  });
 
-  // Start a socket connection to the server
-  socket = io.connect("http://localhost:8080");
+  socket.on("userStatus", function (data) {
+    console.log(data);
+    userNames = []; // Reset the array to avoid duplicates
+    for (let i = 0; i < data.length; i++) {
+      userNames.push(data[i].playerName);
+    }
+    console.log("Users " + userNames.join(", "));
 
+    // userNames = data.map((user) => user.playerName).join(", ");
+  });
   socket.on("mouse", function (data) {
     console.log("Got: " + data.x + " " + data.y);
+    // Draw a blue circle
     noStroke();
     ellipse(data.x, data.y, 20, 20);
     ellipses.push({
       x: data.x,
       y: data.y,
-      targetY: 490,
+      targetY: 550,
       color: color(0, 0, 255),
     });
+    // IncomingMessage = "Someone added a circle";
   });
-
   socket.on("message", function (data) {
-    console.log("Got message: " + data);
-    IncomingMessage = "Anonymous: " + data;
+    console.log(data);
+    IncomingMessage = data;
     incomingBubble = true;
   });
 
-  inputBox = createInput("Enter text here");
-  inputBox.position(30, height + 40);
-
-  submitButton = createButton("Submit");
-  submitButton.position(inputBox.x + inputBox.width + 10, height + 40);
-  submitButton.mousePressed(handleSubmit);
-
-  function handleSubmit() {
-    OutgoingMessage = inputBox.value();
-    socket.emit("message", OutgoingMessage);
-    outgoingBubble = true;
-    inputBox.value("");
-  }
-
-  inputBox.mousePressed(() => {
-    inputBox.value("");
+  socket.on("playerName", function (data) {
+    console.log(data);
+    player2 = data;
   });
 
+  socket.on("userAdded", function (data) {
+    users.push({
+      playerName: data,
+      playerPosition: users.length == 1 ? "Player 2" : "Player 1",
+    });
+    console.log(users);
+    // Store the user's name in the socket object
+    socket.userName = data;
+    // socket.broadcast.emit("userAdded", data); // send to everyone except sender
+    //  socket.emit("message", data);    // send to sender only    
+  });
+
+  socket.on("disconnect", function () {
+    console.log(socket.userName + " has disconnected");
+    // Find the index of the user with the matching player name
+    const userIndex = users.findIndex(
+      (user) => user.playerName === socket.userName
+    );
+    // Remove the user if found
+    if (userIndex !== -1) {
+      users.splice(userIndex, 1);
+    }
+    console.log(users);
+  });
+
+  socket.on("userExists", function () {
+    console.log("User already exists");
+    alert("User already exists");
+    userName = prompt("Please enter your name:"); ///  help!!!!!!!!!!!!!!!!!!!!!!!
+    manifest = `Welcome to the P5.js Socket.IO Chat App, ${userName}!`;
+    socket.emit("addNewUser", userName);
+  });
+
+
+  inputBox = createInput("Enter text here");
+  inputBox.position(30, height + 40); // Position it below the canvas
+  submitButton = createButton("Submit");
+  submitButton.position(inputBox.x + inputBox.width + 10, height + 40); // Position it next to the input box
+  submitButton.mousePressed(handleSubmit); // Attach a callback function to handle button press
+  inputBox.mousePressed(() => {
+    // Clear input box text when clicked
+    inputBox.value("");
+  });
   inputBox.elt.addEventListener("keypress", (event) => {
+    // Enter act like clicking thebutton
     if (event.key === "Enter") {
       handleSubmit();
     }
   });
 }
 
-function showCustomPrompt() {
-  const customPrompt = document.getElementById("custom-prompt");
-  const customPromptInput = document.getElementById("custom-prompt-input");
-  const customPromptOk = document.getElementById("custom-prompt-ok");
-  const customPromptCancel = document.getElementById("custom-prompt-cancel");
-
-  customPrompt.style.display = "flex";
-
-  customPromptInput.addEventListener("input", () => {
-    customPromptOk.disabled = customPromptInput.value.trim() === "";
-  });
-
-  customPromptOk.addEventListener("click", () => {
-    userInput = customPromptInput.value;
-    customPrompt.style.display = "none";
-  });
-
-  customPromptCancel.addEventListener("click", () => {
-    window.location.href = "http://chriscalver.com";
-  });
-}
-
 function draw() {
+  background(0); // Clear the canvas with a background color
+  textFont("Arial");
+  fill("white");
+  textSize(18);
+  text(IncomingMessage, 35, 55);
+  text(OutgoingMessage, 235, 155);
+  
   background(0);
-
   if (headerBubble) {
-    fill(1, 133, 225);
+    fill(1, 133, 225); // text color blue 
     noStroke();
     text(manifest, 20, 20, 250, 100);
-  }
+    text(userHeader, 20, 130);    
+// make the following text dynamic
+// lets view the following text vertically
+for (let i = 0; i < userNames.length; i++) {
+  text(userNames[i], 20, position + i * 20);
+}
 
+    
+  }
   if (outgoingBubble) {
-    text(OutgoingMessage, 20, 375);
+    noStroke();
+    fill(255, 255, 255); // text color white
+    text(userName + " said: " + OutgoingMessage, 20, 375);
     noStroke();
   }
-
   if (incomingBubble) {
+    noStroke();
+    fill(255, 255, 255); // text color white
     text(IncomingMessage, 20, 255);
     noStroke();
   }
-
-  fill(255);
-  textSize(32);
-  text(userInput, 50, 50);
-
+  // Update and draw each ellipse
   for (let i = 0; i < ellipses.length; i++) {
     let e = ellipses[i];
     if (e.y < e.targetY) {
-      e.y += 5;
+      e.y += 5; // Adjust the speed of the animation here
     }
     fill(e.color);
     noStroke();
     ellipse(e.x, e.y, 20, 20);
   }
+}
+
+function handleSubmit() {
+  let inputValue = inputBox.value();
+  console.log(userName + " Submitted: " + inputValue);
+  OutgoingMessage = inputValue;
+  socket.emit("message", userName + " said: " + inputValue);
+  // socket.emit("playerName", userName);
+  outgoingBubble = true;
+  inputBox.value("");
+}
+
+function mouseClicked() {
+  if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
+    return;
+  }
+  sendmouse(mouseX, mouseY);
+  // Add the ellipse to the array with its initial and target positions
+  ellipses.push({
+    x: mouseX,
+    y: mouseY,
+    targetY: random(490, 400),
+    color: color(255, 255, 0),
+  });
+}
+// Function for sending to the socket
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+  // Make a little object with x and y
+  var data = {
+    x: xpos,
+    y: ypos,
+  };
+  // Send that object to the socket
+  socket.emit("mouse", data);
 }
--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var app = express();
 var http = require("http");
 var cors = require("cors");
 const { Server } = require("socket.io");
+const { emit } = require("process");
 app.use(cors());
-
 const server = http.createServer(app);
 app.use(express.static("public"));
-
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -15,30 +14,74 @@ const io = new Server(server, {
   },
 });
 
-io.sockets.on(
-  "connection",
-  // We are given a websocket object in our function
-  function (socket) {
-    console.log("We have a new client: " + socket.id);
+let users = [];
+let tempUser = "";
 
-    socket.on("mouse", function (data) {     
-      console.log("Received: 'mouse' " + data.x + " " + data.y);    
-      socket.broadcast.emit("mouse", data); // send to everyone except sender
-      // socket.emit('mouse', data);    // send to sender only
-    });
+io.sockets.on("connection", function (socket) {
 
-    socket.on("message", function (data) {
-      console.log("Msg: " + data);
-      socket.broadcast.emit("message", data); // send to everyone except sender
+  socket.on("sendName", function (data) {
+    tempUser = data;
+    console.log("Received: 'sendName' " + data);
+  });
+
+  console.log("We have a new client: " + tempUser);
+  // users.push({
+  //   playerName: tempUser,
+  //   playerPosition: "not selected",
+  //   socketId: socket.id,
+  // });
+  console.log(users);
+
+  socket.on("mouse", function (data) {
+    console.log("Received: 'mouse' " + data.x + " " + data.y);
+    socket.broadcast.emit("mouse", data); // send to everyone except sender
+    // socket.emit('mouse', data);    // send to sender only
+  });
+
+  socket.on("message", function (data) {
+    console.log(data);
+    socket.broadcast.emit("message", data); // send to everyone except sender
+    //  socket.emit("message", data);    // send to sender only
+  });
+
+  socket.on("addNewUser", function (data) {
+    console.log(data + " has joined the chat");
+    if (users.some((user) => user.playerName === data)) {
+      console.log("user already exists");
+      socket.emit("userExists", data);
+    } else {
+      users.push({
+        playerName: data,
+        playerPosition: "not selected",
+        socketId: socket.id,
+      });
+      console.log(users);
+      socket.broadcast.emit("userAdded", data); // send to everyone except sender
+      io.sockets.emit('userStatus', users); // send to everyone
+      // io.sockests.emit("userStatus", users); // send to eveyone
+      // io.sockets.emit('users_count', clients);
       //  socket.emit("message", data);    // send to sender only
-    });
+    }
+  });
 
-    socket.on("disconnect", function () {
-      console.log("Client has disconnected");
-    });
-  }
-);
+  socket.on("disconnect", function (data) {
+    console.log(data);
+    // users.pop();
+    if (users.length === 0) {
+      console.log("no users");
+      return;
+    } else {
+      console.log("users exist");
+      let userIndex = users.findIndex((user) => user.socketId === socket.id);
+      console.log(users[userIndex].playerName + " has disconnected");
+      users = users.filter((user) => user.socketId !== socket.id);
+      io.sockets.emit('userStatus', users); // send to everyone
+    }
+    // console.log("has disconnected");
+    console.log(users);
+  });
+});
 
 server.listen(8080, () => {
-  console.log("listening on *:8080");
+  console.log("listening on port 8080");
 });
